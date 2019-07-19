e4fd195f5952810dc4e075c7f2b809cd5c1d4ded
Şamil Korkmaz <samil.korkmaz@gmail.com>
2019-07-19 16:07:13 +0300

    Refactoring: More comments and helper functions

--- main.cpp
+++ main.cpp
@@ -6,9 +6,6 @@
 #include<opencv2/imgproc/imgproc.hpp>
 
 #include<iostream>
-#ifdef WINDOWS
-#include<conio.h>           // it may be necessary to change or remove this line if not using Windows
-#endif
 
 #include "Blob.h"
 
@@ -19,140 +16,118 @@ const cv::Scalar SCALAR_BLUE = cv::Scalar(255.0, 0.0, 0.0);
 const cv::Scalar SCALAR_GREEN = cv::Scalar(0.0, 200.0, 0.0);
 const cv::Scalar SCALAR_RED = cv::Scalar(0.0, 0.0, 255.0);
 
+void showContours(cv::Mat image, std::vector<std::vector<cv::Point> > contours, cv::String name) {
+	cv::Mat imgContours(image.size(), CV_8UC3, SCALAR_BLACK);
+	cv::drawContours(imgContours, contours, -1, SCALAR_WHITE, -1);
+	cv::imshow(name, imgContours);
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int main(void) {
+	cv::VideoCapture capVideo(OPENCV_FOLDER"..\\..\\..\\sources\\samples\\data\\vtest.avi");
+	//cv::VideoCapture capVideo("C:/Users/sam/Downloads/YBSYeniHatTest3005.mp4");
+	if (!capVideo.isOpened()) { // if unable to open video file
+		std::cout << "\nerror reading video file" << std::endl << std::endl;      
+		getchar();                    
+		return(-1);                                                              
+	}
+	if (capVideo.get(cv::CAP_PROP_FRAME_COUNT) < 2) {
+		std::cout << "\nerror: video file must have at least two frames";
+		getchar();
+		return(-2);
+	}
+	cv::Mat frame1;
+	cv::Mat frame2;
+	capVideo.read(frame1);
+	capVideo.read(frame2);
 
-    cv::VideoCapture capVideo;
+	const char ESC_KEY = 27; 
+	char keyPressed = 0;
+	while (capVideo.isOpened() && keyPressed != ESC_KEY) {
+		std::vector<Blob> blobs;
 
-    cv::Mat imgFrame1;
-    cv::Mat imgFrame2;
+		cv::Mat frame1Copy = frame1.clone();
+		cv::Mat frame2Copy = frame2.clone();
 
-    capVideo.open("768x576.avi");
+		//convert to grayscale:
+		cv::cvtColor(frame1Copy, frame1Copy, cv::COLOR_BGR2GRAY);
+		cv::cvtColor(frame2Copy, frame2Copy, cv::COLOR_BGR2GRAY);
 
-    if (!capVideo.isOpened()) {                                                 // if unable to open video file
-        std::cout << "\nerror reading video file" << std::endl << std::endl;      // show error message
-        #ifdef WINDOWS
-        _getch();                    // it may be necessary to change or remove this line if not using Windows
-        #endif
-        return(0);                                                              // and exit program
-    }
+		//blur both frames to prevent small differences like ribbons moving with wind from being detected
+		cv::Size blurSize = cv::Size(5, 5);
+		double sigmaX = 0;
+		cv::GaussianBlur(frame1Copy, frame1Copy, blurSize, sigmaX);
+		cv::GaussianBlur(frame2Copy, frame2Copy, blurSize, sigmaX);
 
-    if (capVideo.get(CV_CAP_PROP_FRAME_COUNT) < 2) {
-        std::cout << "\nerror: video file must have at least two frames";
-        #ifdef WINDOWS
-        _getch();
-        #endif
-        return(0);
-    }
+		cv::Mat frameDiff;
+		cv::absdiff(frame1Copy, frame2Copy, frameDiff); //difference between two frames
 
-    capVideo.read(imgFrame1);
-    capVideo.read(imgFrame2);
+		double tresh = 30; double maxVal = 255;
+		cv::Mat frameThresh;
+		cv::threshold(frameDiff, frameThresh, tresh, maxVal, cv::THRESH_BINARY); //diff values that are above threshold of 30
 
-    char chCheckForEscKey = 0;
+		cv::imshow("frameThresh", frameThresh); //show differences that are above treshold
 
-    while (capVideo.isOpened() && chCheckForEscKey != 27) {
+		//remove noise and settle down imperfections:
+		cv::Mat structuringElement5x5 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
+		cv::dilate(frameThresh, frameThresh, structuringElement5x5); //increase size, filling holes
+		//cv::imshow("imgThreshDilate1", imgThresh);
+		
+		//get contours of treshold image:
+		cv::Mat imgThreshCopy = frameThresh.clone();
+		std::vector<std::vector<cv::Point> > treshContours;
+		cv::findContours(imgThreshCopy, treshContours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+		//show contours of threshold image:
+		showContours(frameThresh, treshContours, "contours");
 
-        std::vector<Blob> blobs;
+		//form convex hulls:
+		std::vector< std::vector<cv::Point> > convexHullsContours(treshContours.size());
+		for (unsigned int i = 0; i < treshContours.size(); i++) {
+			cv::convexHull(treshContours[i], convexHullsContours[i]);
+		}
+		for (auto &convexHull : convexHullsContours) {
+			Blob possibleBlob(convexHull);
+			//if convexHull satisfies the following geometric criteria, add it to list
+			if (possibleBlob.boundingRect.area() > 100 &&
+				possibleBlob.dblAspectRatio >= 0.2 &&
+				possibleBlob.dblAspectRatio <= 1.2 &&
+				possibleBlob.boundingRect.width > 15 &&
+				possibleBlob.boundingRect.height > 20 &&
+				possibleBlob.dblDiagonalSize > 30.0) {
+				blobs.push_back(possibleBlob);
+			}
+		}
 
-        cv::Mat imgFrame1Copy = imgFrame1.clone();
-        cv::Mat imgFrame2Copy = imgFrame2.clone();
+		//show satistactory convex hulls: TODO make this into a function and use it also in above contour showing section.
+		convexHullsContours.clear();
+		for (auto &blob : blobs) {
+			convexHullsContours.push_back(blob.contour);
+		}
+		showContours(frameThresh, convexHullsContours, "convexHulls");
 
-        cv::Mat imgDifference;
-        cv::Mat imgThresh;
+		//get boxes around convex hulls:
+		frame2Copy = frame2.clone(); // get another copy of frame2 since we changed the previous frame2 copy in the processing above
+		for (auto &blob : blobs) {                                                  
+			cv::rectangle(frame2Copy, blob.boundingRect, SCALAR_RED, 2);      // draw a red box around the blob
+			cv::circle(frame2Copy, blob.centerPosition, 3, SCALAR_GREEN, -1); // draw a filled-in green circle at the center
+		}
+		cv::imshow("boxes", frame2Copy);
 
-        cv::cvtColor(imgFrame1Copy, imgFrame1Copy, CV_BGR2GRAY);
-        cv::cvtColor(imgFrame2Copy, imgFrame2Copy, CV_BGR2GRAY);
+		// now we prepare for the next iteration
+		frame1 = frame2.clone();  // move frame1 up to where frame2 is
+		if ((capVideo.get(cv::CAP_PROP_POS_FRAMES) + 1) < capVideo.get(cv::CAP_PROP_FRAME_COUNT)) { // if there is at least one more frame
+			capVideo.read(frame2);  // read next frame
+		}
+		else {                                                  
+			std::cout << "end of video\n";                      
+			break;  // and jump out of while loop
+		}
+		keyPressed = cv::waitKey(1);
+	}
 
-        cv::GaussianBlur(imgFrame1Copy, imgFrame1Copy, cv::Size(5, 5), 0);
-        cv::GaussianBlur(imgFrame2Copy, imgFrame2Copy, cv::Size(5, 5), 0);
-
-        cv::absdiff(imgFrame1Copy, imgFrame2Copy, imgDifference);
-
-        cv::threshold(imgDifference, imgThresh, 30, 255.0, CV_THRESH_BINARY);
-
-        cv::imshow("imgThresh", imgThresh);
-
-        cv::Mat structuringElement3x3 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
-        cv::Mat structuringElement5x5 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
-        cv::Mat structuringElement7x7 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(7, 7));
-        cv::Mat structuringElement9x9 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9));
-
-        cv::dilate(imgThresh, imgThresh, structuringElement5x5);
-        cv::dilate(imgThresh, imgThresh, structuringElement5x5);
-        cv::erode(imgThresh, imgThresh, structuringElement5x5);
-
-        cv::Mat imgThreshCopy = imgThresh.clone();
-
-        std::vector<std::vector<cv::Point> > contours;
-
-        cv::findContours(imgThreshCopy, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-        cv::Mat imgContours(imgThresh.size(), CV_8UC3, SCALAR_BLACK);
-
-        cv::drawContours(imgContours, contours, -1, SCALAR_WHITE, -1);
-
-        cv::imshow("imgContours", imgContours);
-
-        std::vector<std::vector<cv::Point> > convexHulls(contours.size());
-
-        for (unsigned int i = 0; i < contours.size(); i++) {
-            cv::convexHull(contours[i], convexHulls[i]);
-        }
-
-        for (auto &convexHull : convexHulls) {
-            Blob possibleBlob(convexHull);
-
-            if (possibleBlob.boundingRect.area() > 100 &&
-                possibleBlob.dblAspectRatio >= 0.2 &&
-                possibleBlob.dblAspectRatio <= 1.2 &&
-                possibleBlob.boundingRect.width > 15 &&
-                possibleBlob.boundingRect.height > 20 &&
-                possibleBlob.dblDiagonalSize > 30.0) {
-                blobs.push_back(possibleBlob);
-            }
-        }
-
-        cv::Mat imgConvexHulls(imgThresh.size(), CV_8UC3, SCALAR_BLACK);
-
-        convexHulls.clear();
-
-        for (auto &blob : blobs) {
-            convexHulls.push_back(blob.contour);
-        }
-
-        cv::drawContours(imgConvexHulls, convexHulls, -1, SCALAR_WHITE, -1);
-
-        cv::imshow("imgConvexHulls", imgConvexHulls);
-
-        imgFrame2Copy = imgFrame2.clone();          // get another copy of frame 2 since we changed the previous frame 2 copy in the processing above
-
-        for (auto &blob : blobs) {                                                  // for each blob
-            cv::rectangle(imgFrame2Copy, blob.boundingRect, SCALAR_RED, 2);             // draw a red box around the blob
-            cv::circle(imgFrame2Copy, blob.centerPosition, 3, SCALAR_GREEN, -1);        // draw a filled-in green circle at the center
-        }
-
-        cv::imshow("imgFrame2Copy", imgFrame2Copy);
-
-                // now we prepare for the next iteration
-
-        imgFrame1 = imgFrame2.clone();           // move frame 1 up to where frame 2 is
-
-        if ((capVideo.get(CV_CAP_PROP_POS_FRAMES) + 1) < capVideo.get(CV_CAP_PROP_FRAME_COUNT)) {       // if there is at least one more frame
-            capVideo.read(imgFrame2);                            // read it
-        }
-        else {                                                  // else
-            std::cout << "end of video\n";                      // show end of video message
-            break;                                              // and jump out of while loop
-        }
-
-        chCheckForEscKey = cv::waitKey(1);      // get key press in case user pressed esc
-
-    }
-
-    if (chCheckForEscKey != 27) {               // if the user did not press esc (i.e. we reached the end of the video)
-        cv::waitKey(0);                         // hold the windows open to allow the "end of video" message to show
-    }
-    // note that if the user did press esc, we don't need to hold the windows open, we can simply let the program end which will close the windows
-
-    return(0);
+	if (keyPressed != ESC_KEY) { // if the user did not press esc (i.e. we reached the end of the video)
+		cv::waitKey(0);  // hold the windows open to allow the "end of video" message to show
+	}
+	// note that if the user did press esc, we don't need to hold the windows open, we can simply let the program end which will close the windows
+	return(0);
 }
